1d2597454c995c6e473c53818b5d8cd37399821b
nicoguerini <39567513+nicoguerini@users.noreply.github.com>
2018-11-14 10:45:47 -0500

    Update GroupsG.java

--- GroupsG.java
+++ GroupsG.java
@@ -24,14 +24,6 @@ import java.awt.SystemColor;
 public class GroupsG {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private static ArrayList<String> studentNames = new ArrayList<String>();
-	private static ArrayList<String> studentNamesCopy = new ArrayList<String>();
-	private static String[] alpha = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-	private static String[] key;
 	
 	/**
 	 * Launch the application.
