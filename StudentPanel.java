549654666de5c9a5466be9eb924fa266dba83b7d
20sleibowitz <44878090+20sleibowitz@users.noreply.github.com>
2018-11-19 17:13:32 -0500

    Add files via upload

---
+++ StudentPanel.java
@@ -0,0 +1,101 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+public class StudentPanel implements ItemListener {
+
+	public Student student; 
+	public JPanel panel;
+	public JLabel nameLabel;
+	private JComponent frame; 
+
+	public JComboBox<String> firstChoiceList;
+	public JComboBox<String> secondChoiceList; 
+	public JComboBox<String> thirdChoiceList;
+	
+	public static int x = 50;
+	public static int y = 50;
+	
+	public StudentPanel(Student s){
+		student = s;
+		nameLabel = new JLabel(student.name);
+		panel = new JPanel();
+		firstChoiceList = new JComboBox(s.choice1List.toArray());
+		secondChoiceList = new JComboBox(s.choice2List.toArray());
+		thirdChoiceList = new JComboBox(s.choice3List.toArray());
+		firstChoiceList.addItemListener(this);
+		secondChoiceList.addItemListener(this);
+		thirdChoiceList.addItemListener(this);
+		
+		initialize(x,y);
+		y+=100;
+	}
+	
+	public void initialize(int x, int y){
+		panel.setBounds(x, y, 704, 98);
+		panel.setLayout(null);
+		
+		nameLabel.setBounds(33, 31, 101, 41);
+		panel.add(nameLabel);
+		
+		firstChoiceList.setBounds(154, 31, 160, 40);
+		panel.add(firstChoiceList);
+		
+		secondChoiceList.setBounds(334, 31, 160, 40);
+		panel.add(secondChoiceList);
+		
+		thirdChoiceList.setBounds(509, 31, 160, 40);
+		panel.add(thirdChoiceList);
+
+	}
+	
+
+	@Override
+	public void itemStateChanged(ItemEvent event) {
+		if(event.getSource().equals(firstChoiceList)){
+		/*	
+			if(firstChoiceList.getSelectedItem().equals("RESET LIST")){
+				firstChoiceList = new JComboBox(student.choice1List.toArray());
+				firstChoiceList.setBounds(154, 31, 160, 40);
+				firstChoiceList.setSelectedIndex(0);
+			}
+			*/
+			student.choice1 = (String) firstChoiceList.getSelectedItem(); 
+			secondChoiceList.removeItem(student.choice1);
+			thirdChoiceList.removeItem(student.choice1);
+			
+			
+		}
+		else if(event.getSource().equals(secondChoiceList)){
+		/*	
+			if(secondChoiceList.getSelectedItem().equals("RESET LIST")){
+				secondChoiceList = new JComboBox(student.choice2List.toArray());
+				secondChoiceList.setBounds(334, 31, 160, 40);
+				secondChoiceList.setSelectedIndex(0);
+			}
+			*/
+				student.choice2 = (String) secondChoiceList.getSelectedItem();
+				firstChoiceList.removeItem(student.choice2);
+				thirdChoiceList.removeItem(student.choice2);
+
+			
+			
+		}
+		else if(event.getSource().equals(thirdChoiceList)){
+		/*
+			if(thirdChoiceList.getSelectedItem().equals("RESET LIST")){
+				thirdChoiceList = new JComboBox(student.choice3List.toArray());
+				thirdChoiceList.setBounds(509, 31, 160, 40);
+				thirdChoiceList.setSelectedIndex(0);
+			}
+			*/
+			student.choice3 = (String) thirdChoiceList.getSelectedItem();
+			firstChoiceList.removeItem(student.choice3);
+			secondChoiceList.removeItem(student.choice3);
+			
+			
+		}
+	}
+
+
+}
---
+++ TeacherMode.java
@@ -0,0 +1,112 @@
+import javax.swing.*;
+import java.util.*;
+import java.awt.Dimension;
+import java.awt.event.*;
+
+//*************************
+//THIS USES THREE CLASSES: 
+//TeacherMode
+//StudentPanel
+//and Student
+//*************************
+
+public class TeacherMode implements ActionListener {
+
+	private JFrame frame;
+	private JPanel bigPanel;
+	private JScrollPane scroll;
+	private JButton enter = new JButton("ENTER CHOICES");
+	private String[] names = {" ", "Amy", "Bob", "Carla", "Dave", "Erinn", "Frank", "Greg", "Hannah"}; 
+	public ArrayList<String> studentNames = new ArrayList<String>(Arrays.asList(names));
+	//names will not exist in final version, it is just there so that I could input the names for testing easily
+	//studentNames will have to not be a static variable in the real thing when it is attached to the other part
+	public int numStudes;
+	public ArrayList<StudentPanel> panelList;
+	public ArrayList<Student> students = new ArrayList<Student>();
+	public ArrayList<String[]> resultArray;
+	
+	public static void main(String[] args){
+		TeacherMode t = new TeacherMode();
+	}
+	
+	public TeacherMode() {
+		//studentNames.add("RESET LIST");
+		numStudes = studentNames.size()-1;
+		resultArray = new ArrayList<String[]>();
+		enter.addActionListener(this);
+		initialize();
+		createStudentObjects();
+		createPanels();
+		
+	}
+
+	private void initialize() {
+		frame = new JFrame("TEACHER MODE");
+		frame.setBounds(0, 0, 825, 900);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setVisible(true);
+		bigPanel = new JPanel();
+		bigPanel.setLayout(null);
+		bigPanel.setPreferredSize(new Dimension(1000, 1000));
+		scroll = new JScrollPane(bigPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scroll.setBounds(0, 0, 800, 900);
+		scroll.setVisible(true);
+		frame.add(scroll);
+		
+		JLabel firstChoicelbl = new JLabel("FIRST CHOICE");
+		JLabel secondChoicelbl = new JLabel("SECOND CHOICE");
+		JLabel thirdChoicelbl = new JLabel("THIRD CHOICE");
+		firstChoicelbl.setBounds(230, 26, 100, 40);
+		secondChoicelbl.setBounds(415, 26, 100, 40);
+		thirdChoicelbl.setBounds(600, 26, 100, 40);
+		bigPanel.add(firstChoicelbl);
+		bigPanel.add(secondChoicelbl);
+		bigPanel.add(thirdChoicelbl);
+		
+
+	}
+	
+	private void createStudentObjects(){
+		for(int x = 0; x < numStudes; x++){
+			students.add(new Student(studentNames, x+1));
+		}
+		for(int x = 0; x <numStudes; x++){
+			//students.get(x).setChoice1List();
+			//students.get(x).setChoice2List();
+			//students.get(x).setChoice3List();
+		}
+	}
+	
+	private void createPanels(){
+		panelList = new ArrayList<StudentPanel>();
+		for(int x = 0; x < numStudes; x++){
+			panelList.add(new StudentPanel(students.get(x)));
+		}
+		for(int x = 0; x < numStudes; x++){
+			bigPanel.add(panelList.get(x).panel);
+			}
+		enter.setBounds(354, StudentPanel.y +20, 189, 43);
+		bigPanel.add(enter);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if(event.getSource().equals(enter)){
+			for(int x = 0; x < numStudes; x++){
+				String[] temp = new String[4];
+				temp[0] = panelList.get(x).student.name;
+				temp[1] = (String) panelList.get(x).firstChoiceList.getSelectedItem();
+				temp[2] = (String) panelList.get(x).secondChoiceList.getSelectedItem();
+				temp[3] = (String) panelList.get(x).thirdChoiceList.getSelectedItem();
+				resultArray.add(temp);
+
+			}
+			
+		}
+		
+	}
+
+
+	
+}
